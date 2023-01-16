1ac69d2d6446209d63fbed6ced0f393d718de9de
safhin <mdjahidhasan920@gmail.com>
2023-01-16 10:19:09 +0600

    Solve another problem

---
+++ sameArray.js
@@ -0,0 +1,54 @@
+//if each element of the first array is equal to of the next arrays each element then the result will be true, false otherwise
+
+
+//way 1
+function sameArray(arr1,arr2){
+
+    let trueCount = 0;
+
+    if(arr1.length === arr2.length){
+        for (let index = 0; index < arr1.length; index++) {
+            for (let j = 0; j < arr2.length; j++) {
+                if(arr1[index] * arr1[index] === arr2[j]){
+                    arr2.splice(j,1)
+                    
+                }
+            }
+        }
+
+        if(arr2.length <= 0){
+            return true
+        }else{
+            return false
+        }
+        
+    }else{
+        return false;
+    }
+}
+
+//way 2
+function sameArray2(arr1,arr2){
+
+    let trueCount = 0;
+
+    if(arr1.length === arr2.length){
+        for (let index = 0; index < arr1.length; index++) {
+            let currentIndex = arr2.indexOf(arr1[index] ** 2);
+
+            if(currentIndex === -1){
+                return false
+            }
+
+            arr2.splice(currentIndex,1)
+
+            return true
+        }
+        
+    }else{
+        return false;
+    }
+}
+
+
+console.log(sameArray2([0,2,3,6,10],[9,100,0,4,36]))
